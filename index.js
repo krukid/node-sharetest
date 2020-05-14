914ed7415073aede1bd2e80d8ece33fcfa01b929
Viktors Buls <krukid@gmail.com>
2020-05-14 22:22:49 +0300

    add request logging

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.set('view engine', 'pug')
 
 app.use(express.static('static'))
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
 app.get('/crop', function(req, res) {
   const {url, crop, gravity} = req.query
   gm(request(url))
